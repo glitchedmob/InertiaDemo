e5b7e361723061c22be98ffec3e41f149a3bfb38
Levi Zitting <me@levizitting.com>
2022-12-16 13:53:10 -0600

    initial commit

---
+++ InertiaDemo.Backend/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using InertiaAdapter;
+using Microsoft.AspNetCore.Mvc;
+using InertiaDemo.Backend.Models;
+using Microsoft.AspNetCore.Components.RenderTree;
+
+namespace InertiaDemo.Backend.Controllers;
+
+public class HomeController : Controller
+{
+    public HomeController()
+    {
+    }
+
+    public IActionResult Index()
+    {
+        return Inertia.Render("Home", new { title = "Home" });
+    }
+}
